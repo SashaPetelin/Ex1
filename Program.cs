547fc07d66a85d26a5b24a745fa3fcd2f0079d5d
Sasha Petelin <alexanderzr58@icloud.com>
2022-09-12 22:43:49 +0300

    Example#19

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите 5-ти значное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+GetPal(number);
+
+void GetPal(int number)
+{
+    while (number >= 0)
+    {
+        int m = number % 10;
+        int secnumber = number / 10;
+        sum = sum * 10 + m;
+    }
+    if (sum == number) Console.WriteLine("Число - палиндром!");
+    else Console.WriteLine("Число -не палиндром!");
+}
